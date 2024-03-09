2c8e419979e95c3cfac5de35fcc25ce7d740e2a9
diegodev9 <diegodev9@gmail.com>
2024-03-09 20:07:30 -0300

    add turbo stream for line_items

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order(:title)
+    session[:counter] = session[:counter].nil? ? 0 : session[:counter] + 1
   end
 end
