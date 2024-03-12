2d963f756a62caab4416b32f91518f2372308a6f
diegodev9 <diegodev9@gmail.com>
2024-03-12 12:30:18 -0300

    added stimulus to order

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -7,4 +7,10 @@ module CurrentCart
     @cart = Cart.create
     session[:cart_id] = @cart.id
   end
+
+  def ensure_cart_isnt_empty
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty'
+    end
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,5 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:title)
-    session[:counter] = session[:counter].nil? ? 0 : session[:counter] + 1
   end
 end
